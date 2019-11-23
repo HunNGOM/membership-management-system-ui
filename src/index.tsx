a5a4b6afe920a6863e6e8725434378891e4540c3
szabototo89 <szabototo89@gmail.com>
2019-11-23 17:45:29 +0100

    add hungarian language

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { Application } from './application';
 import { BrowserRouter } from 'react-router-dom';
+import { LanguageContext } from './language-context';
+import { hungarianLanguageLabels } from './language-context/hungarian-language-labels';
 
 ReactDOM.render(
   <BrowserRouter>
-    <Application />
+    <LanguageContext.Provider value={hungarianLanguageLabels}>
+      <Application />
+    </LanguageContext.Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
